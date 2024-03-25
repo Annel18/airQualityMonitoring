d79fd5ac806b28cc116672c2543749614f8907b3
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-03-25 15:01:35 +0000

    setup Jest and React Testing Library in Vite project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import './styles/App.css'
+import "./styles/App.css";
+import React from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setTodos(json));
+  }, []);
 
   return (
     <>
-      <h1>Hello World!</h1>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <h1>{todo.title}</h1>
+          <p>{todo.completed}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
