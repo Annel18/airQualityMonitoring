cc66bc1efcd0b516e3a81b18d836fd86024d29df
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-04-10 16:26:56 +0100

    App jsx set up to use navigation and outlet from reac-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+//! Imports
+import { Spinner } from 'react-bootstrap'
+import { Outlet, useNavigation } from 'react-router-dom'
+
+import Header from './components/header'
+
+
 export default function App() {
+  //! States
+  const navigation = useNavigation()
+  //! Effects
 
+  //! Functions
 
-    return (
-        <>
-            <h1>Hello World!</h1>
-            <p>test</p>
+  //! JSX
+  return (
+    <>
+      <Header />
+      <main>
+        {
+          navigation.state === 'idle' ?
+            <Outlet />
+            :
+            <section className='spinner'>
+              <Spinner />
+            </section>
+        }
+      </main>
 
-        </>
-    );
+    </>
+  );
 }
