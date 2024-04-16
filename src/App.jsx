1d3af718272225c13b1928220e73cc574062f5c6
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-04-16 13:14:32 +0100

    footer component started

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Footer from "./components/Footer"
+
 export default function App() {
 
 
@@ -5,6 +7,7 @@ export default function App() {
         <>
             <h1>Hello World!</h1>
             <p>test</p>
+            <Footer/>
 
         </>
     );
