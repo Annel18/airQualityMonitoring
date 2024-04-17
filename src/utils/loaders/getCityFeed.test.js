839e09e4eea03e369f2defde23f2cb5e045ee84b
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-04-17 15:06:48 +0100

    loaders and test for loader function created

--- src/utils/loaders/getCityFeed.test.js
+++ src/utils/loaders/getCityFeed.test.js
@@ -1,4 +1,27 @@
-const problems = require('./getCityFeed')
-test('first test to Add function', () => {
-    expect(problems.add(1,2)).toEqual(3)
+import { getCityFeed } from './getCityFeed'
+
+describe('getCityFeed function', () => {
+  // Mocking fetch
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({ mockData: 'mockValue' }),
+    })
+  )
+
+  beforeEach(() => {
+    fetch.mockClear()
+  })
+
+  it('should call fetch with the correct URL', async () => {
+    const token = 'mockToken'
+    const expectedURL = `https://api.waqi.info/feed/here/?token=${token}`
+    await getCityFeed(token)
+    expect(fetch).toHaveBeenCalledWith(expectedURL)
+  })
+
+  it('should return JSON data from the response', async () => {
+    const token = 'mockToken'
+    const result = await getCityFeed(token)
+    expect(result).toEqual({ mockData: 'mockValue' })
+  })
 })
