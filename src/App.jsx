6b1fd182c560feca148f68894742ed6a40327207
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-04-17 11:34:27 +0100

    Revert "Revert "Navbar header component started""
    This reverts commit c2cadb8008e773f29e3b2cc4e0808e000f501bc4.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Spinner } from 'react-bootstrap'
 import { Outlet, useNavigation } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
+import Footer from "./components/Footer"
 
 
 export default function App() {
