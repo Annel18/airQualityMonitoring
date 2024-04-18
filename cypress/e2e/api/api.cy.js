edcf0bb11348f44255e4b6ee48a2244cb36befcd
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-04-18 18:54:41 +0100

    api.cy.js file created

---
+++ cypress/e2e/api/api.cy.js
@@ -0,0 +1,16 @@
+
+// About API testing: https://docs.cypress.io/api/commands/request#Method-and-URL
+describe("Check https://api.publicapis.org/entries request", () => {
+    it("Get 200 status", () => {
+        cy.request({
+            method: "GET",
+            url: `https://api.publicapis.org/entries`,
+        }).as("getEntries");
+
+        cy.get("@getEntries").should((response) => {
+            expect(response.status).to.eq(200);
+            expect(response).to.have.property("headers");
+            expect(response.body).to.have.property("entries")
+        });
+    });
+});
