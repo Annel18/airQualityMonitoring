44e288157f6f9b217f6f69854743e2c2928bcd7d
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-04-23 17:25:16 +0100

    optimizDeps no longer needed

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    include: ['esm-dep > cjs-dep'],
-  },
   plugins: [react()],
   server: {
     host: true,
