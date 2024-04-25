696f95c3af86884816e11f5965671cdc60d2f17d
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-04-25 12:22:40 +0100

    App test updated to be render withing BrowserRouter

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,11 +1,14 @@
 import { render, screen } from "@testing-library/react";
-
+import { BrowserRouter } from "react-router-dom"
 import App from "./App";
 
 describe("App", () => {
-
     it("renders App component", async () => {
-        render(<App />);
+        render(
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        )
         const linkElement = await screen.findByText(/air/i);
         expect(linkElement).toBeInTheDocument();
     });
