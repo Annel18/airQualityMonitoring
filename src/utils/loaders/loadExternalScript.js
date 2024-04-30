60ea945395fadb9fb0c9d92aef3cdb30eddf474b
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-04-30 23:49:09 +0100

    loadExternalScript.js created

---
+++ src/utils/loaders/loadExternalScript.js
@@ -0,0 +1,13 @@
+export default function loadExternalScript(w, d, t, f) {
+  w[f] = w[f] || function (c, k, n) {
+    const s = w[f];
+    k = s['k'] = (s['k'] || (k ? ('&k=' + k) : ''));
+    s['c'] = c = (c instanceof Array) ? c : [c];
+    s['n'] = n = n || 0;
+    const L = d.createElement(t);
+    const e = d.getElementsByTagName(t)[0];
+    L.async = 1;
+    L.src = '//feed.aqicn.org/feed/' + (c[n].city) + '/' + (c[n].lang || '') + '/feed.v1.js?n=' + n + k;
+    e.parentNode.insertBefore(L, e);
+  };
+}
