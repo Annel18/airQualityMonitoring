4ee09f54f151df56b7c8f9b2ba04932a95dc5fb2
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-05-03 19:53:08 +0100

    code reverted

--- src/components/PageRealTime/PageRealTime.jsx
+++ src/components/PageRealTime/PageRealTime.jsx
@@ -1,26 +1,13 @@
-import React, { useState } from "react"
 import { useLoaderData } from "react-router-dom"
 import WidgetDetails from "../Widget/WidgetDetails"
-import SearchBar from "../SearchBar/SearchBar"
 
 export default function PageRealTime() {
-  const [location, setLocation] = useState('here')
   const data = useLoaderData()
-  // Function to handle location change
-  const handleLocationChange = (newLocation) => {
-    setLocation(newLocation)
-  }
   return (
     <>
-      <SearchBar onLocationChange={handleLocationChange} /> {/* Pass callback function */}
-      <h1>Your Searched Data</h1>
-      <div>
-        <WidgetDetails location={location} />
-      </div>
-      <h1>Your Local Data</h1>
-      <div>
-        <WidgetDetails location={'here'} />
-      </div>
+      <h1>Real Time DATA</h1>
+      <h2>DATA status = {data.status}</h2>
+      <WidgetDetails/>
     </>
   )
 }
