2bf31acc7adc6636252b770651ce709d71187f0c
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-05-05 16:36:54 +0100

    footer branch converted into Typescript

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,12 +1,16 @@
 import { render, screen } from "@testing-library/react"
-// import '@testing-library/jest-dom/extend-expect'
+import { BrowserRouter } from "react-router-dom"
 
 import App from "./App"
 
 describe("App", () => {
     it("renders App component", async () => {
-        render(<App />)
-        const linkElement = await screen.findByText(/air/i)
-        expect(linkElement).toBeInTheDocument()
+        render(
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        )
+        const linkElement = await screen.findByText(/air/i);
+        expect(linkElement).toBeInTheDocument();
     })
 })
