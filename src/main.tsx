ba07518ecf68a6124e9823286747fd3ec32bbdaa
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-05-05 16:23:20 +0100

    development branch converted into Typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 //! Components
-import App from './components/App/index.js'
-import PageRealTime from './components/PageRealTime/index.js'
-import PageForecast from './components/PageForecast/index.js'
+import App from './components/App/index.ts'
+import PageRealTime from './components/PageRealTime/index.ts'
+import PageForecast from './components/PageForecast/index.ts'
 
 //! Loaders
-import { getCityFeed } from './utils/loaders/getCityFeed.js'
+import { getCityFeed } from './utils/loaders/getCityFeed.ts'
 
 //! Styles
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -42,4 +41,4 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
   
-)
+) as HTMLElement
