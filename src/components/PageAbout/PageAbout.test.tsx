be0e82478f25001f3dc5189b159d944f788fd6f3
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-05-09 15:49:44 +0100

    conflict resolution

--- src/components/PageAbout/PageAbout.test.tsx
+++ src/components/PageAbout/PageAbout.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import PageAbout from './index'
+import PageAbout from "./index"
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import '@testing-library/jest-dom/extend-expect'
