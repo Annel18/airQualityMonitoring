a43d6e8a7489b39d848f6c3e8dbc0d89b6b3157c
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-05-29 13:31:45 +0100

    getCityFeed.ts update with LoaderFunctionArgs types corrected and test adjusted

--- src/utils/loaders/getCityFeed.ts
+++ src/utils/loaders/getCityFeed.ts
@@ -1,8 +1,18 @@
-import axios from 'axios'
+import axios from 'axios';
+import { LoaderFunctionArgs } from 'react-router-dom';
 
-export async function getCityFeed(location: string) {
-  const token = process.env.API_KEY
-  // const url = process.env.API_URL_LOCAL
-  const response = await axios.get(`https://api.waqi.info/feed/${location}/?token=${token}`)
-  return response.data
-}
+export const getCityFeed = async (input: LoaderFunctionArgs | string) => {
+  const token = process.env.API_KEY;
+
+  let location: string;
+  if (typeof input === 'string') {
+    location = input;
+  } else {
+    const { request, params } = input;
+    const url = new URL(request.url);
+    location = params.location || url.searchParams.get('location') || 'defaultLocation';
+  }
+
+  const response = await axios.get(`https://api.waqi.info/feed/${location}/?token=${token}`);
+  return response.data;
+};
