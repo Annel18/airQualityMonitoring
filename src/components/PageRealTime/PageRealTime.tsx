e46a1996fde956ee81ecfad37b29ac59b5c175c2
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2024-05-30 17:41:37 +0100

    PageRealtime simplified

--- src/components/PageRealTime/PageRealTime.tsx
+++ src/components/PageRealTime/PageRealTime.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import WidgetDetails from "../Widget/WidgetDetails";
 import SearchBar from "../SearchBar/SearchBar";
 import DataRealTime from "../DataRealTime/DataRealTime";
 
@@ -15,14 +14,6 @@ const PageRealTime: React.FC = () => {
       <div className="page-realtime">
         <div className="map-large">The MAP will come here</div>
         <DataRealTime key={searchedLocation} location={searchedLocation} />
-        {/* <h1>Your Searched Data</h1> */}
-        {/* <div>
-        <WidgetDetails key={searchedLocation} location={searchedLocation} />
-      </div> */}
-        {/* <h1>Your Local Data</h1>
-      <div>
-        <WidgetDetails key={'here'} location={'here'} />
-      </div> */}
       </div>
     </>
   )
